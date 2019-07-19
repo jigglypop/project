dd7d2fc084b7c4b00dfbf86932fdf998f4d046b3
jigglypop <ydh2244@gmail.com>
2019-07-19 18:42:36 +0900

    안녕

---
+++ movie.py
@@ -0,0 +1,28 @@
+import csv
+import requests
+
+key = '100055495b6ed905789d9f0ddf560f3f'
+movies = []
+with open('boxoffice.csv', 'r', encoding='utf-8') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        movies.append(row['영화코드'])
+        
+result = {}
+for movieCd in movies:
+    response = requests.get(f'http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?movieCd={movieCd}&key={key}').json()
+    movie_info = response['movieInfoResult']['movieInfo']
+    result[movieCd] = {
+            '영화 대표코드': movie_info.get('movieCd'),
+            '영화명(국문)': movie_info.get('movieNm'),
+            '영화명(영문)': movie_info.get('movieNmEn'),
+            '영화명(원문)': movie_info.get('movieNmOg'),
+            '관람등급': movie_info.get('audits')[0]['watchGradeNm'] if movie_info.get('audits') else None,
+            '장르': movie_info.get('genres')[0]['genreNm'] if movie_info.get('genres') else None,
+        }
+with open('movie.csv', 'w', encoding='utf-8') as f:
+    fieldnames = ['영화 대표코드' , '영화명(국문)' , '영화명(영문)' , '영화명(원문)' , '관람등급' , '개봉연도' , '상영시간' , '장르' , '감독명'] # 맞춰서 작성..
+    csv_writer = csv.DictWriter(f, fieldnames=fieldnames)
+    csv_writer.writeheader()
+    for item in result.values():
+        csv_writer.writerow(item)
